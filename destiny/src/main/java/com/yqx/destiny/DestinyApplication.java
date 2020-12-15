1d614c2465b03363b76101c9e375b3447fc77bd1
yanqiuxiang <271785410@qq.com>
2020-12-15 16:50:29 +0800

    init

---
+++ destiny/src/main/java/com/yqx/destiny/DestinyApplication.java
@@ -0,0 +1,13 @@
+package com.yqx.destiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinyApplication.class, args);
+	}
+
+}
---
+++ destiny/src/test/java/com/yqx/destiny/DestinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqx.destiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
