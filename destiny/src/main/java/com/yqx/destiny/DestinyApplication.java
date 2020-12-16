d6abbbd4581e5947e82c2da866e9e20f0834aeb3
yanqiuxiang <271785410@qq.com>
2020-12-16 11:33:46 +0800

    destiny

--- destiny/src/main/java/com/yqx/destiny/DestinyApplication.java
+++ destiny/src/main/java/com/yqx/destiny/DestinyApplication.java
@@ -1,13 +1,28 @@
 package com.yqx.destiny;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DestinyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DestinyApplication.class, args);
 	}
 
+	@Slf4j
+	@RestController
+	static class TestController {
+
+		@GetMapping("/hello")
+		public String hello(String name) {
+			log.info("name:{}", name);
+			return "hello " + name;
+		}
+	}
 }
