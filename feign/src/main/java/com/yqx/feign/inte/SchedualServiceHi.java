2f2b4add598a255c17efad863085312a59ea5497
yanqiuxiang <271785410@qq.com>
2020-12-18 17:00:30 +0800

    熔断器

--- feign/src/main/java/com/yqx/feign/inte/SchedualServiceHi.java
+++ feign/src/main/java/com/yqx/feign/inte/SchedualServiceHi.java
@@ -1,12 +1,13 @@
 package com.yqx.feign.inte;
 
 
+import com.yqx.feign.inte.fallback.SchedualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "nacos-server")
+@FeignClient(value = "nacos-server",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
