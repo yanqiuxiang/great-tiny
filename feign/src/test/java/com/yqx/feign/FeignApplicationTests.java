d69c05cd74c8471fa5ae0d6030cd83e504e3855e
yanqiuxiang <271785410@qq.com>
2020-12-21 09:24:33 +0800

    熔断器

---
+++ feign/src/test/java/com/yqx/feign/FeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqx.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
